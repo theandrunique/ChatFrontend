f3c0630713a21de6eff1a777f192c499314b98b8
theandrunique <theandrunique@gmail.com>
2024-04-10 00:48:10 +0300

    Add UI logic for auth page

--- ChatFrontend/Pages/Auth.xaml.cs
+++ ChatFrontend/Pages/Auth.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChatFrontend.Pages
 {
@@ -24,45 +14,103 @@ namespace ChatFrontend.Pages
         {
             InitializeComponent();
         }
+        bool isButtonDown = false;
+        bool isMouseOver = false;
+        SolidColorBrush defaultColor = new SolidColorBrush(Color.FromRgb(116, 53, 156));
+        SolidColorBrush mouseOverColor = new SolidColorBrush(Color.FromRgb(94, 47, 124));
+        SolidColorBrush mouseDownColor = new SolidColorBrush(Color.FromRgb(80, 44, 102));
 
-        private void AuthPage_Loaded(object sender, RoutedEventArgs e)
+        SolidColorBrush errorColor = new SolidColorBrush(Color.FromRgb(239, 49, 36));
+
+        private void SignIn_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            isMouseOver = true;
+            if (!isButtonDown)
+                signIn.Background = mouseOverColor;
         }
-
-        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        private void signIn_MouseLeave(object sender, MouseEventArgs e)
         {
-
-        }
-
-        private void login_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void password_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-
+            isMouseOver = false;
+            if (!isButtonDown)
+                signIn.Background = defaultColor;
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            isButtonDown = true;
+            signIn.Background = mouseDownColor;
         }
-
-        private void SignIn_MouseEnter(object sender, MouseEventArgs e)
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
         {
-
+            signIn_MouseUp(null, null);
         }
-
-        private void signIn_MouseLeave(object sender, MouseEventArgs e)
-        {
-
-        }
-
         private void signIn_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            isButtonDown = false;
+            if (isMouseOver)
+                signIn.Background = mouseOverColor;
+            else
+                signIn.Background = defaultColor;
 
+            if (isMouseOver)
+            {
+                ClickSignIn();
+            }
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ClickSignIn();
+            }
+        }
+        private void ClickSignIn()
+        {
+            string loginValue = login.Text;
+            if (string.IsNullOrEmpty(loginValue))
+                SetLoginError("Login cannot be empty!");
+            string passwordValue = password.Password;
+            if (string.IsNullOrEmpty(passwordValue))
+                SetPasswordError("Password cannot be empty!");
+
+            MessageBox.Show("Click counted!");
+            SetPasswordError("- Some very very long error that cannot be shown in window\n- overflow overflow\n- overflow overflow overflow overflow overflow");
+        }
+
+        private void AuthPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window.KeyDown += Window_KeyDown;
+        }
+        private void SetLoginError(string error)
+        {
+            loginError.Text = error;
+            loginBorder.BorderBrush = errorColor;
+        }
+        private void ClearLoginError()
+        {
+            loginBorder.BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            loginError.Text = null;
+        }
+        private void ClearPasswordError()
+        {
+            passwordBorder.BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            passwordError.Text = null;
+        }
+        private void SetPasswordError(string error)
+        {
+            passwordError.Text = error;
+            passwordBorder.BorderBrush = errorColor;
+        }
+
+        private void login_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            ClearLoginError();
+        }
+
+        private void password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            ClearPasswordError();
         }
     }
 }

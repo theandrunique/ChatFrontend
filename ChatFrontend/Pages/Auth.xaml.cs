28fb657aabadfcde35a7303189a3598260615680
theandrunique <theandrunique@gmail.com>
2024-04-10 00:45:08 +0300

    Add a new auth page

---
+++ ChatFrontend/Pages/Auth.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatFrontend.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void login_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void SignIn_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void signIn_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void signIn_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}

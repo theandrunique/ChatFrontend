fced7f6749b0d1bb1380e2ff74da428d1000fd98
theandrunique <theandrunique@gmail.com>
2024-04-12 00:50:52 +0300

    Update components

--- ChatFrontend/Components/MyMessage.xaml.cs
+++ ChatFrontend/Components/MyMessage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatFrontend.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,12 +17,12 @@ using System.Windows.Shapes;
 namespace ChatFrontend.Components
 {
     /// <summary>
-    /// Логика взаимодействия для Message.xaml
+    /// Логика взаимодействия для MyMessage.xaml
     /// </summary>
-    public partial class Message : UserControl
+    public partial class MyMessage : UserControl
     {
         Classes.Message currentMessage;
-        public Message(Classes.Message message)
+        public MyMessage(Classes.Message message)
         {
             InitializeComponent();
             currentMessage = message;

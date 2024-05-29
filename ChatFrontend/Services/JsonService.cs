978ddef4d2cc4d957d9b18dc4ed12c9a73454d88
theandrunique <theandrunique@gmail.com>
2024-05-29 14:17:48 +0300

    Add implementation for IJsonService interface

---
+++ ChatFrontend/Services/JsonService.cs
@@ -0,0 +1,21 @@
+﻿using ChatFrontend.Services.Base;
+using Newtonsoft.Json;
+
+namespace ChatFrontend.Services
+{
+    public class JsonService : IJsonService
+    {
+        public string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+        public object Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject(json);
+        }
+        public T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}

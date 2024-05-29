34f8887aafa3a9314f12190f29cefc2005207509
theandrunique <theandrunique@gmail.com>
2024-05-29 14:17:12 +0300

    Add IJsonService interface

---
+++ ChatFrontend/Services/Base/IJsonService.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatFrontend.Services.Base
+{
+    interface IJsonService
+    {
+        string Serialize(object obj);
+        object Deserialize(string json);
+        T Deserialize<T>(string json);
+    }
+}

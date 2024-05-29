3e162af6a14205f8e124bfe900a182d93b17ccc2
theandrunique <theandrunique@gmail.com>
2024-05-29 17:19:38 +0300

    Add navigation service

---
+++ ChatFrontend/Services/Base/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using ShopContent.ViewModels.Base;
+
+namespace ChatFrontend.Services.Base
+{
+    public interface INavigationService
+    {
+        ViewModel CurrentView { get; }
+        void NavigateTo<T>() where T : ViewModel;
+    }
+}
---
+++ ChatFrontend/Services/NavigationService.cs
@@ -0,0 +1,31 @@
+﻿using ChatFrontend.Services.Base;
+using ShopContent.ViewModels.Base;
+using System;
+
+namespace ChatFrontend.Services
+{
+    public class NavigationService : ViewModel, INavigationService
+    {
+        private readonly Func<Type, ViewModel> _viewModelFactory;
+        private ViewModel _currentView;
+        public ViewModel CurrentView
+        {
+            get => _currentView;
+            private set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+        public NavigationService(Func<Type, ViewModel> viewModelFactory)
+        {
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public void NavigateTo<T>() where T : ViewModel
+        {
+            var viewModel = _viewModelFactory.Invoke(typeof(T));
+            CurrentView = viewModel;
+        }
+    }
+}

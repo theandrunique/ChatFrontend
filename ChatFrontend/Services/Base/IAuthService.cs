02ce06f18eb43f8bf4a758db6ba293e46bd6d934
theandrunique <theandrunique@gmail.com>
2024-05-30 10:00:23 +0300

    Add IAuthService interface

---
+++ ChatFrontend/Services/Base/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace ChatFrontend.Services.Base
+{
+    interface IAuthService
+    {
+        Task<string> Login(string login, string password);
+        Task<bool> SignUp(string username, string email, string password);
+    }
+}

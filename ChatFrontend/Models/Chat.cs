525975e741dfcfd502f3c28dddc183dda694b43b
theandrunique <theandrunique@gmail.com>
2024-06-05 22:58:40 +0300

    Add chat and message models

---
+++ ChatFrontend/Models/Chat.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+
+namespace ChatFrontend.Models
+{
+    public class Chat
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("creator_id")]
+        public string CreatorId { get; set; }
+
+        [JsonProperty("members")]
+        public List<string> Members { get; set; }
+
+        [JsonProperty("admins")]
+        public List<string> Admins { get; set; }
+
+        [JsonProperty("message_count")]
+        public int MessageCount { get; set; }
+
+        [JsonProperty("last_message")]
+        public Message LastMessage { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
---
+++ ChatFrontend/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ChatFrontend.Models
+{
+    public class Message
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("from_id")]
+        public string FromId { get; set; }
+
+        [JsonProperty("attachments")]
+        public List<string> Attachments { get; set; }
+    }
+}

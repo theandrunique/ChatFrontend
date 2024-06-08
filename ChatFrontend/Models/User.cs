94d1b6c545560a980c654ca96843b58d3eae23d7
theandrunique <theandrunique@gmail.com>
2024-06-08 10:53:21 +0300

    Add viem model for user module

--- ChatFrontend/Models/User.cs
+++ ChatFrontend/Models/User.cs
@@ -1,8 +1,21 @@
-﻿namespace ChatFrontend.Models
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ChatFrontend.Models
 {
     public class User
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+        [JsonProperty("username")]
         public string Username { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("email_verified")]
+        public bool EmailVerified { get; set; }
+        [JsonProperty("active")]
+        public bool Active { get; set; }
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
     }
 }
---
+++ ChatFrontend/ViewModels/UserCardVM.cs
@@ -0,0 +1,41 @@
+﻿using ChatFrontend.Models;
+using ShopContent.Commands;
+using ShopContent.ViewModels.Base;
+using System;
+using System.CodeDom.Compiler;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChatFrontend.ViewModels
+{
+    public class UserCardVM : ViewModel
+    {
+        private User _user;
+
+        public User User
+        {
+            get
+            {
+                return _user;
+            }
+            set
+            {
+                _user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+
+        public ICommand SendMessageCommand { get; }
+
+        public UserCardVM(User user)
+        {
+            User = user;
+            SendMessageCommand = new LambdaCommand(ExecuteSendMessage);
+        }
+
+        private void ExecuteSendMessage(object obj)
+        {
+            MessageBox.Show(User.Email);
+        }
+    }
+}

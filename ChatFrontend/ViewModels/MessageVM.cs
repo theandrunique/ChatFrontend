d69809372b121e63403ca76d6c934ee93eb5ca2d
theandrunique <theandrunique@gmail.com>
2024-06-12 11:42:10 +0300

    Add view model for message

---
+++ ChatFrontend/ViewModels/MessageVM.cs
@@ -0,0 +1,44 @@
+﻿using ChatFrontend.Models;
+using ShopContent.ViewModels.Base;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatFrontend.ViewModels
+{
+    public class MessageVM : ViewModel
+    {
+        private Message _message;
+        private Chat _chat;
+        private ICollection<User> _members;
+
+        public Message Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+        public Chat Chat
+        {
+            get => _chat;
+            set
+            {
+                _chat = value;
+                OnPropertyChanged(nameof(Chat));
+            }
+        }
+        public User Sender
+        {
+            get => _members.FirstOrDefault(u => u.Id == _message.FromId);
+        }
+
+        public MessageVM(Message message, Chat chat, ICollection<User> members)
+        {
+            Message = message;
+            Chat = chat;
+            _members = members;
+        }
+    }
+}

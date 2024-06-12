c6d039dc2f159aa11d196c443a524d6bc0baaa22
theandrunique <theandrunique@gmail.com>
2024-06-12 13:05:47 +0300

    Update settings page

--- ChatFrontend/ViewModels/SettingsVM.cs
+++ ChatFrontend/ViewModels/SettingsVM.cs
@@ -1,12 +1,59 @@
-﻿using ShopContent.ViewModels.Base;
+﻿using ChatFrontend.Models;
+using ChatFrontend.Services.Base;
+using Microsoft.Win32;
+using ShopContent.Commands;
+using ShopContent.ViewModels.Base;
+using System.IO;
+using System.Windows.Input;
+using ChatFrontend.Core;
 
 namespace ChatFrontend.ViewModels
 {
     public class SettingsVM : ViewModel
     {
-        public SettingsVM()
-        {
+        private readonly IAuthService _authService;
+        private readonly IStorageService _storageService;
 
+        private User _user;
+
+        public User User
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+
+        public ICommand ChangeProfilePictureCommand { get; }
+
+        public SettingsVM(IAuthService authService, IStorageService storageService, AppState appState)
+        {
+            User = appState.User;
+            _authService = authService;
+            _storageService = storageService;
+            ChangeProfilePictureCommand = new LambdaCommand(ExecuteChangeProfilePicture);
+        }
+
+        private async void ExecuteChangeProfilePicture(object obj)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileName = Path.GetFileName(filePath);
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    var response = await _storageService.PutFiles(fileStream);
+                    User.ImageUrl = response.Files[fileName];
+
+                    await _authService.UpdateImageUrl(response.Files[fileName]);
+                    OnPropertyChanged(nameof(User));
+                }
+            }
         }
     }
 }

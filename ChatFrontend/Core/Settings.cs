ac151e07add2142431221b666bd4535bd51854c9
theandrunique <theandrunique@gmail.com>
2024-06-16 07:16:24 +0300

    Update config

--- ChatFrontend/Core/Settings.cs
+++ ChatFrontend/Core/Settings.cs
@@ -5,5 +5,8 @@
         public string AuthServiceBaseUrl { get; set; }
         public string MessengerServiceBaseUrl { get; set; }
         public string MessengerWebSocketUrl { get; set; }
+        public string AuthServiceClientId { get; set; }
+        public string AuthServiceClientSecret { get; set; }
+        public string AuthServiceRedirectUri { get; set; }
     }
 }
